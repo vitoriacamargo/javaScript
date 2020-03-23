9ba098933a3d84cd7cbbe93a7d9c5d256263c179
Vitoria Camargo da Silva <48717189+vitoriacamargo@users.noreply.github.com>
2020-03-23 11:57:29 -0300

    Create aula-7.js

---
+++ aula-7.js
@@ -0,0 +1,31 @@
+var convidados = [
+    {
+      nome: 'Pedro',
+      idade: 20,
+      bebeRefrigerante: false,
+      bebeCerveja: true
+    },
+    {
+      nome: 'Maria',
+      idade: 17,
+      bebeRefrigerante: true,
+      bebeCerveja: false
+    },
+    {
+      nome: 'João',
+      idade: 19,
+      bebeRefrigerante: true,
+      bebeCerveja: true
+    }
+  ]
+   
+  var comprarCerveja = false
+   
+  for (var i = 0; i <= convidados.length; i++) {
+    if (convidados[i].bebeCerveja) {
+      comprarCerveja = true
+      break
+    }
+  }
+   
+  console.log(comprarCerveja)
