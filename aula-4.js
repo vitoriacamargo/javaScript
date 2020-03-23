b3f75d70522214bbab5952ec2fc2576093919dca
Vitoria Camargo da Silva <48717189+vitoriacamargo@users.noreply.github.com>
2020-03-23 11:07:48 -0300

    Create aula-4.js

---
+++ aula-4.js
@@ -0,0 +1,16 @@
+var nomes = [
+  'joão',
+  'maria',
+  'pedro'
+]
+ 
+var pessoa = {
+  nome: 'Bruno',
+  amigos: [
+    'joão',
+    'maria',
+    'pedro'
+  ]
+}
+ 
+console.log(pessoa.amigos[1])
