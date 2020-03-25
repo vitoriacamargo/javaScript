698c1ea6f6f46cbe5294dcd4f7a41db783dce49b
Vitoria Camargo da Silva <48717189+vitoriacamargo@users.noreply.github.com>
2020-03-25 11:10:13 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,5 +1,17 @@
-console.log("Fuicarregado no arquivo externo");
-
-var titulo = document.querySelector("h1");
-
+var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
