b2c39e9b7784eb564ee96e82cfae0c4a50449949
Vitoria Camargo da Silva <48717189+vitoriacamargo@users.noreply.github.com>
2020-03-30 17:29:30 -0300

    Add files via upload

--- principal.js
+++ principal.js
@@ -1,36 +1,39 @@
-var  titulo = document.querySelector(".titulo");
+var titulo = document.querySelector(".titulo");
+//titulo.textContent = "Aparecida Nutricionista";
 
-var pacientes = document.querySelectorAll(".paciente");
+var paciente = document.querySelector(".paciente");
 
-for (var i = 0; i < pacientes.length; i++) {
+for(var i = 0; i< pacientes.lenght ; i++){
 
-    var paciente = pacientes[i];
+var paciente = paciente[i];
 
-    var tdPeso = paciente.querySelector(".info-peso");
-    var peso = tdPeso.textContent;
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
 
-    var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
 
-    var tdImc = paciente.querySelector(".info-imc");
+var tdImc = paciente.querySelector(".info-imc");
 
-    var pesoEhValido = true;
-    var alturaEhValida = true;
+var pesoEhValido = true;
+var alturaEhValida = true;
 
-    if (peso <= 0 || peso >= 1000) {
-        console.log("Peso inv·lido!");
-        pesoEhValido = false;
-        tdImc.textContent = "Peso inv·lido";
-    }
-
-    if (altura <= 0 || altura >= 3.00) {
-        console.log("Altura inv·lida!");
-        alturaEhValida = false;
-        tdImc.textContent = "Altura inv·lida";
-    }
-
-  if (alturaEhValida && pesoEhValido) {
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc;
-    }
+if (peso < 0 || peso > 1000) {
+    console.log("Peso inv√°lido!");
+    pesoEhValido = false;
 }
+
+if (altura <= 0 || altura >= 3.00) {
+    console.log("Altura inv√°lida!");
+    alturaEhValida = false;
+    tdImc.textContent= "Altura inv√°lida!";
+    paciente.classList.add("paciente-invalido");
+}
+
+if( alturaEhValida && pesoEhValido){
+    var imc = peso/ (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+  }
+}
+var imc = peso / (altura * altura);
+tdImc.textContent = imc;
